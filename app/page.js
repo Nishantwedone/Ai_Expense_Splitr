3b72bec63dfb0ff12ad049c5811ba192c9ad68e3
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2025-11-15 15:09:33 +0530

    middleware configuration done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return <div className="p-16">
-    Hello World
-    <br />
-    <Button variant="destructive">Click me</Button>
     
-    </div>;
+  </div>;
 }
--- convex/schema.js
+++ convex/schema.js
@@ -2,6 +2,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 
+
 export default defineSchema({  
     users: defineTable({
         name: v.string(),
@@ -13,5 +14,54 @@ export default defineSchema({
     .index("by_email", ["email"])
     .searchIndex("search_name", { searchField: "name" })
     .searchIndex("search_email", { searchField: "email" }),
-});
 
+    expenses: defineTable({
+        description: v.string(),
+        amount: v.number(),
+        category: v.optional(v.string()),
+        date: v.number(),
+        paidByUserId: v.id("users"),
+        splitType: v.string(), // e.g., "equal", "unequal", "percentage"
+        splits: v.array(
+            v.object({
+                userId: v.id("users"),
+                amount: v.number(),
+                paid: v.boolean(),
+            })
+        ),
+        groupId: v.optional(v.id("groups")),
+        createdBy: v.id("users"),
+    })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_date", ["date"]),
+
+
+    groups: defineTable({
+        name: v.string(),
+        description: v.optional(v.string()),
+        createdBy: v.id("users"),
+        members: v.array(
+            v.object({
+                userId: v.id("users"),
+                role: v.string(), // e.g., "admin", "member"
+                joinedAt: v.number(),
+            })
+        ),
+    }),
+
+    settlements: defineTable({
+        amount: v.number(),
+        note: v.optional(v.string()),
+        date: v.number(),
+        paidByUserId: v.id("users"),
+        receivedByUserId: v.id("users"),
+        groupId: v.optional(v.id("groups")),
+        relatedExpenseId: v.union(v.null(), v.array(v.id("expenses"))),
+        createdBy: v.id("users"),
+    })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_receiver_and_group", ["receivedByUserId", "groupId"])
+    .index("by_date", ["date"]),
+});

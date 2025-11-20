2d6e653ee3875a937584085000f9844dc566836c
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2025-11-20 19:22:20 +0530

    automatic AI Mail remainder set mothly and Group expense UI done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { he } from "date-fns/locale";
 import { paymentReminders } from "@/lib/inngest/payment-reminders";
+import { spendingInsights } from "@/lib/inngest/spending-insights";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -9,5 +10,6 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     paymentReminders,
+    spendingInsights,
   ],
 });
